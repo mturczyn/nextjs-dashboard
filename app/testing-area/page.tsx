6db5630c3c031b37694c6a382c6e8aca70997504
Michal Turczyn <turek1992@o2.pl>
2024-07-22 22:24:25 +0200

    Added example of server action.

--- app/testing-area/page.tsx
+++ app/testing-area/page.tsx
@@ -1,6 +1,14 @@
 import Link from 'next/link'
+import { Button } from '../ui/button'
 
 export default function Page() {
+    const serverAction = async () => {
+        'use server'
+        console.log(
+            'This log should be visible on server side. During local debugging, should be visible in Visual Studio Code, or other tool used to run the applicaitn.'
+        )
+    }
+
     return (
         <>
             <p className="m-2">
@@ -13,6 +21,20 @@ export default function Page() {
             >
                 Go to example of client component trying to import SSR component
             </Link>
+            <p className="m-2">
+                Below is small test how to invoke server actoin through
+                submitting form, as form accepts now server action in{' '}
+                <code>action</code> attribute.
+            </p>
+            <p className="m-2">
+                Clicking button will log message to console. By using server
+                action log will be visible on server side, not in browser.
+            </p>
+            <form action={serverAction}>
+                <Button type="submit" className="m-2">
+                    Call server action
+                </Button>
+            </form>
         </>
     )
 }
